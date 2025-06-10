64544d8bd571f76b2b78ce2ee52d230d39bdfcf5
PortgasDRopy <117282513+PortgasDRopy@users.noreply.github.com>
2025-06-10 16:47:19 +0200

    tentative journal

--- Cap'îlot/Assets/Sauvegarde/Sauvegarde.cs
+++ Cap'îlot/Assets/Sauvegarde/Sauvegarde.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 using System.IO;
 using static UnityEngine.Rendering.DebugUI;
 using TMPro;
+using UnityEngine.UI;
 
 public class Sauvegarde : MonoBehaviour
 {
     private Journal journal;
     [SerializeField] private TextMeshProUGUI Input;
+    [SerializeField] private TMP_Dropdown Output;
+    [SerializeField] private TextMeshProUGUI OutputText;
     // Start is called before the first frame update
 
     private void Awake()
@@ -17,20 +20,50 @@ public class Sauvegarde : MonoBehaviour
         journal = new Journal();
         try
         {
-            string jsonstring = File.ReadAllText("C:/Users/ederveaux/Documents/GitHub/Cap-ilot/Cap'îlot/save.json");
+            string jsonstring = File.ReadAllText("save.json");
             journal = JsonUtility.FromJson<Journal>(jsonstring);
+            Output.ClearOptions();
+            List<string> keyList = new List<string>();
+            foreach (string key in journal.journal.Keys)
+            {
+                keyList.Add(key);
+            }
+            Output.AddOptions(keyList);
+            Output.RefreshShownValue();
+            OutputText.text = journal.journal[Output.captionText.ToString()];
         }
         catch
         {
 
         }
         journal.InputField = Input;
+        Output.onValueChanged.AddListener(delegate {
+            DropdownValueChanged(Output);
+        });
     }
     public void Save()
     {
+        Output.ClearOptions();
         journal.Save();
         string jsonString = JsonUtility.ToJson(journal);
         string fileName = "save.json";
         File.WriteAllText(fileName, jsonString);
+        List<string> keyList = new List<string>();
+        foreach (string key in journal.journal.Keys)
+        {
+            keyList.Add(key);
+        }
+        Output.AddOptions(keyList);
+/*        foreach (Dropdown.OptionData option in Output.options)
+        {
+            option. = journal.journal[option.text];
+        }*/
+        Output.RefreshShownValue();
+    }
+
+    void DropdownValueChanged(TMP_Dropdown change)
+    {
+        print(Output.captionText.ToString());
+        OutputText.text = journal.journal[Output.captionText.ToString()];
     }
 }
