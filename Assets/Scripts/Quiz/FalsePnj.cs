59dc265ad3e9d133dc658b5346a637906efceea1
doriansimonet <dsimonet@gaming.tech>
2025-06-16 12:01:03 +0200

    Update : answering question

---
+++ Assets/Scripts/Quiz/FalsePnj.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FalsePnj : MonoBehaviour
+{
+
+    private int id;
+    [SerializeField] QuizManager QuizManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (QuizManager.allQuestions != null)
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Quiz/Quiz.cs
+++ Assets/Scripts/Quiz/Quiz.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
@@ -11,24 +12,47 @@ public class Quiz : MonoBehaviour
     [SerializeField] private GameObject _questionUI;
     [SerializeField] private GameObject _canva;
 
-    private List<string> _answers;
+    private List<string> _answers = new List<string>();
     void Start()
     {
         GameObject uiQuiz = Instantiate(_questionUI,_canva.transform);
         uiQuiz.GetComponentInChildren<TextMeshProUGUI>().text = _questionScriptable.question;
 
-        _answers = _questionScriptable.wrongAnswer;
+        _answers.Add(_questionScriptable.wrongAnswer[0]);
+        _answers.Add(_questionScriptable.wrongAnswer[1]);
+        _answers.Add(_questionScriptable.wrongAnswer[2]);
         _answers.Add(_questionScriptable.correctAnswer);
+
         for (int i=1;i< uiQuiz.GetComponentsInChildren<TextMeshProUGUI>().Length; i++)
         {
             int q = Random.Range(0, _answers.Count);
             uiQuiz.GetComponentsInChildren<TextMeshProUGUI>()[i].text = _answers[q];
+            _answers.RemoveAt(q);
+        }
+
+
+
+        for (int i = 0; i < uiQuiz.GetComponentsInChildren<Button>().Length; i++)
+        {
+            SetButtonOnClickAnswer(uiQuiz.GetComponentsInChildren<Button>()[i]);
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void SetButtonOnClickAnswer(Button button)
     {
-        
+        button.onClick.AddListener(() => CheckAnswer(button));
+    }
+
+    public void CheckAnswer(Button answerChose)
+    {
+        if(answerChose.GetComponentInChildren<TextMeshProUGUI>().text == _questionScriptable.correctAnswer)
+        {
+            answerChose.GetComponentInChildren<Image>().color = Color.green;
+        }
+        else
+        {
+
+            answerChose.GetComponentInChildren<Image>().color = Color.red;
+        }
     }
 }
