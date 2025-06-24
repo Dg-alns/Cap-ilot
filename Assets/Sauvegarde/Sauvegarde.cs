5895f77bb669c7440dd2b42dda9fb5ec354f01fd
PortgasDRopy <117282513+PortgasDRopy@users.noreply.github.com>
2025-06-24 11:28:22 +0200

    Profile

--- Assets/Sauvegarde/Sauvegarde.cs
+++ Assets/Sauvegarde/Sauvegarde.cs
@@ -8,14 +8,13 @@ using TMPro;
 using UnityEngine.UI;
 using static UnityEngine.InputManagerEntry;
 using UnityEditor.UIElements;
+using UnityEngine.SceneManagement;
 
 public class Sauvegarde : MonoBehaviour
 {
     private Journal journal;
     private Profile profile;
     [SerializeField] TextMeshProUGUI Input;
-    [SerializeField] TextMeshProUGUI InputName;
-    [SerializeField] TextMeshProUGUI OutputName;
     [SerializeField] TMP_Dropdown Output;
     [SerializeField] TextMeshProUGUI OutputText;
     [SerializeField] Wheel EmotionWheel;
@@ -26,21 +25,24 @@ public class Sauvegarde : MonoBehaviour
     [SerializeField] UnityEngine.UI.Button Relations;
     [SerializeField] UnityEngine.UI.Button Temptations;
     private List<string> Themes = new List<string>();
+    [SerializeField] TextMeshProUGUI InputName;
+    [SerializeField] TextMeshProUGUI OutputName;
+    [SerializeField] TMP_Dropdown Jour;
+    [SerializeField] TMP_Dropdown Mois;
+    [SerializeField] TMP_Dropdown Annee;
+    [SerializeField] TextMeshProUGUI OutputDate;
     // Start is called before the first frame update
 
     private void Awake()
     {
         journal = new Journal();
         profile = new Profile();
-        journal.Output = Output;
-        journal.EmotionWheel = EmotionWheel;
-        journal.OutputText = OutputText;
-        profile.Output = OutputName;
         try
         {
             string jsonstring = File.ReadAllText("save.json");
             Saving save = new Saving(journal, profile);
             save = JsonUtility.FromJson<Saving>(jsonstring);
+            //SceneManager.LoadScene("Enzo");
             journal = save.journal;
             profile = save.profile;
             journal.Output = Output;
@@ -48,23 +50,26 @@ public class Sauvegarde : MonoBehaviour
             journal.OutputText = OutputText;
             journal.UpdateJournal();
             profile.Output = OutputName;
-            profile.UpdateName();
+            profile.OutputDate = OutputDate;
+            profile.UpdateProfile();
+            journal.InputField = Input;
+            Output.onValueChanged.AddListener(delegate {
+                journal.DropdownValueChanged(Output);
+            });
+            Hospital.onClick.AddListener(() => { OnClick(Hospital); });
+            Food.onClick.AddListener(() => { OnClick(Food); });
+            Sport.onClick.AddListener(() => { OnClick(Sport); });
+            School.onClick.AddListener(() => { OnClick(School); });
+            Relations.onClick.AddListener(() => { OnClick(Relations); });
+            Temptations.onClick.AddListener(() => { OnClick(Temptations); });
         }
         catch
         {
-
+            profile.Input = InputName;
+            profile.Jour = Jour;
+            profile.Mois = Mois;
+            profile.Annee = Annee;
         }
-        journal.InputField = Input;
-        profile.Input = InputName;
-        Output.onValueChanged.AddListener(delegate {
-            journal.DropdownValueChanged(Output);
-        });
-        Hospital.onClick.AddListener(() => { OnClick(Hospital); });
-        Food.onClick.AddListener(() => { OnClick(Food); });
-        Sport.onClick.AddListener(() => { OnClick(Sport); });
-        School.onClick.AddListener(() => { OnClick(School); });
-        Relations.onClick.AddListener(() => { OnClick(Relations); });
-        Temptations.onClick.AddListener(() => { OnClick(Temptations); });
     }
 
     public void OnClick(UnityEngine.UI.Button pressed)
@@ -81,15 +86,16 @@ public class Sauvegarde : MonoBehaviour
     }
     public void Save()
     {
-        Saving save = new Saving(journal, profile);
+        profile.Save();
         journal.ThemeList = Themes;
         journal.Save();
-        profile.Save();
+        Saving save = new Saving(journal, profile);
         string jsonString = JsonUtility.ToJson(save);
         string fileName = "save.json";
         File.WriteAllText(fileName, jsonString);
+        //SceneManager.LoadScene("Enzo");
         journal.UpdateJournal();
-        profile.UpdateName();
+        profile.UpdateProfile();
         Themes.Clear();
     }
 }
