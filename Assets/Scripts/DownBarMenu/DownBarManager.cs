e61b7ceedcb77cb2f56e258249d2e218da245f3d
SirDec73 <cbernard@gaming.tech>
2025-08-01 16:33:10 +0200

    Down Bar

--- Assets/Scripts/DownBarMenu/DownBarManager.cs
+++ Assets/Scripts/DownBarMenu/DownBarManager.cs
@@ -13,8 +13,12 @@ enum PAGE_MARK
 
 public class DownBarManager : MonoBehaviour
 {
+    [SerializeField] private Animator _animator;
+    [SerializeField] private GameObject LockGame;
 
-    [SerializeField] private GameObject LockGame; 
+    [Header("Page")]
+    [SerializeField] private RectTransform PageJournal;
+    [SerializeField] private RectTransform PageProfil;
 
     [Header("Page Mark")]
     [SerializeField] private List<RectTransform> _PageMarks;
@@ -22,7 +26,9 @@ public class DownBarManager : MonoBehaviour
     private float offSet_PageMark = 30f;
     private PAGE_MARK _activePageMark = PAGE_MARK.GAME;
 
-
+    private float currentOffSetPage = 0;
+    private float offSet_Page = 1800f; // Height for a page
+    private float hidePagePosition = -1266f - 900f; // 1266 -> half size screen | 900 -> half offset_Page
 
 
     // Start is called before the first frame update
@@ -36,7 +42,25 @@ public class DownBarManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        AutoPosition_PageMark();
+        _animator.SetInteger("StateDownBar", (int)_activePageMark);
+
+        //AutoPosition_PageMark();
+
+        //AutoPosition_Page();
+    }
+
+    public void AutoPosition_Page()
+    {
+        Debug.Log(PageProfil.position.y);
+        if(Mathf.Abs(PageProfil.position.y + offSet_Page + currentOffSetPage) <= 0.1)
+            return;
+
+        float newPositionY = Mathf.MoveTowards(PageProfil.position.y, - offSet_Page + currentOffSetPage, _speedPageMark * Time.deltaTime);
+
+        Vector3 newPosition = new Vector3(PageProfil.position.x, newPositionY, PageProfil.position.z);
+
+        PageProfil.position = newPosition;
+        PageJournal.position = newPosition;
     }
 
     public void AutoPosition_PageMark()
@@ -44,15 +68,16 @@ public class DownBarManager : MonoBehaviour
         for (int i = 0; i < _PageMarks.Count; i++)
         {
             float newPositionY;
-            float offSet = 0f;
+            float offSet = (currentOffSetPage == 0f) ? 0 : offSet_Page;
 
-            if (i == (int)_activePageMark)
-                offSet = offSet_PageMark;
+            if (i == (int)_activePageMark){
+                offSet += offSet_PageMark;
+            }
 
             if (Mathf.Abs(_PageMarks[i].position.y - offSet) < 0.1)
                 continue;
 
-            newPositionY = Mathf.Lerp(_PageMarks[i].position.y, offSet, _speedPageMark * Time.deltaTime);
+            newPositionY = Mathf.MoveTowards(_PageMarks[i].position.y, offSet, _speedPageMark * Time.deltaTime);
 
             _PageMarks[i].position = new Vector3(_PageMarks[i].position.x, newPositionY, _PageMarks[i].position.z);
         }
@@ -60,13 +85,20 @@ public class DownBarManager : MonoBehaviour
 
     public void ChangeActivePage(int page)
     {
+        Debug.Log(page);
         _activePageMark = (PAGE_MARK)page;
-        if(_activePageMark == PAGE_MARK.GAME)
-        {
-            LockGame.SetActive(false);
-            return;
-        }
-        LockGame.SetActive(true);
-    }
+        //if(_activePageMark == PAGE_MARK.GAME)
+        //{
+        //    LockGame.SetActive(false);
+        //    currentOffSetPage = 0f;
+        //    return;
+        //}
+        //LockGame.SetActive(true);
+        //currentOffSetPage = offSet_Page;
 
+        //bool showPageProfil = _activePageMark == PAGE_MARK.JOURNAL;
+
+        //PageProfil.gameObject.SetActive(showPageProfil);
+        //PageJournal.gameObject.SetActive(!showPageProfil);
+    }
 }
---
+++ Assets/Scripts/DownBarMenu/JournalDownBar.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using TMPro;
+using UnityEngine;
+
+public class JournalDownBar : MonoBehaviour
+{
+    [SerializeField] private Sauvegarde_Minigame sauvegarde_Minigame;
+
+    [Header("Character Values")]
+    [SerializeField] private TextMeshProUGUI _Nickname;
+    // Le personnage
+
+    [Header("Statistique Values")]
+    [SerializeField] private TextMeshProUGUI _NbStars;
+    [SerializeField] private TextMeshProUGUI _NbHours;
+    [SerializeField] private TextMeshProUGUI _NbJournal;
+    [SerializeField] private TextMeshProUGUI _NbQuête;
+
+    private void Start()
+    {
+        string jsonstring = File.ReadAllText("save.json");
+        Saving save = JsonUtility.FromJson<Saving>(jsonstring);
+
+        _Nickname.text = save.profile.Username;
+
+        _NbStars.text = sauvegarde_Minigame.GetTotalStars().ToString();
+    }
+}
