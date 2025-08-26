8d885b2d00b41e2b3885ed9bfe68fdc1c087cd63
Diego <147034575+Dg-alns@users.noreply.github.com>
2025-08-26 09:29:42 +0200

    e

--- Assets/Scripts/Sauvegarde/JSON_Manager.cs
+++ Assets/Scripts/Sauvegarde/JSON_Manager.cs
@@ -3,31 +3,33 @@ using UnityEngine;
 
 public static class JSON_Manager
 {
-    public static void SaveData<T>(string fileName, T data)
-    {
-        string path = Path.Combine(Application.persistentDataPath, fileName + ".json");
-        string json = JsonUtility.ToJson(data, true);
-        File.WriteAllText(path, json);
-    }
-
-    //public static void SaveData<T>(string path, T data)
+    //public static void SaveData<T>(string fileName, T data)
     //{
+    //    string path = Path.Combine(Application.persistentDataPath, fileName + ".json");
     //    string json = JsonUtility.ToJson(data, true);
-    //    File.WriteAllText(Application.dataPath + "/Json/" + path + ".json", json);
+    //    File.WriteAllText(path, json);
     //}
 
-    public static T LoadData<T>(string fileName)
+    public static void SaveData<T>(string path, T data)
     {
-        string path = Path.Combine(Application.persistentDataPath, fileName + ".json");
-
-        string json = File.ReadAllText(path);
-        return JsonUtility.FromJson<T>(json);
-
+        string json = JsonUtility.ToJson(data, true);
+        File.WriteAllText(Application.dataPath + "/Json/" + path + ".json", json);
     }
-    //public static T LoadData<T>(string path)
+
+    //public static T LoadData<T>(string fileName)
     //{
-    //    string json = File.ReadAllText(Application.dataPath + "/Json/" + path + ".json");
+    //    string path = Path.Combine(Application.persistentDataPath, fileName + ".json");
+
+    //    string json = File.ReadAllText(path);
     //    return JsonUtility.FromJson<T>(json);
 
     //}
+
+
+    public static T LoadData<T>(string path)
+    {
+        string json = File.ReadAllText(Application.dataPath + "/Json/" + path + ".json");
+        return JsonUtility.FromJson<T>(json);
+
+    }
 }
